39db058872655a6fc502181f5719a83c73e4fcf9
Fean9r <iacopobreschi@gmail.com>
2014-01-13 17:35:39 +0100

    NPI implemented

--- Arrays/Array.cpp
+++ Arrays/Array.cpp
@@ -19,6 +19,7 @@
 
 #include <iostream>
 #include <string>
+#include <stack>
 #include <vector>
 using namespace std;
 
@@ -30,20 +31,99 @@ strVector ex2 = {"4", "13", "5", "/", "+"};
 strVector ex3 = {"6", "13", "*", "5", "/", "8", "+"};
 
 
+inline bool isInteger(const std::string & s)
+{
+   if(s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false ;
+
+   char * p ;
+   strtol(s.c_str(), &p, 10) ;
+
+   return (*p == 0) ;
+}
+
+inline int whichOp(const std::string & s)
+{
+	int res=0;
+	string op1 ("+");
+	string op2 ("-");
+	string op3 ("*");
+	string op4 ("/");
+    if (s.compare(op1) == 0)
+   		res	=1;
+    if (s.compare(op2) == 0)
+   		res	=2;
+   	if (s.compare(op3) == 0)
+   		res	=3;
+    if (s.compare(op4) == 0)
+   		res	=4;
+
+   return res ;
+}
+
+
 template<class ContainerType>
 void evalRPN(ContainerType& sVec){
-
+	stack <string> numbers;
+	int op1,op2;
 	strVecIter sIter = sVec.begin();
 
 	while (sIter != sVec.end()){
 
-		cout << *sIter << endl;
+		if (isInteger(*sIter)){
+			//cout << *sIter <<" is integer" << endl;
+			numbers.push(*sIter);
+		}
+		else {
+			switch (whichOp(*sIter ))
+			{	
+				case 1: 
+					//cout << "+ operand" << endl;
+					op1=atoi( numbers.top().c_str() );
+					numbers.pop();
+					op2=atoi( numbers.top().c_str() );
+					numbers.pop();
+					numbers.push( to_string(op2 + op1));
+	    		break;
+				case 2: 
+					//cout << "- operand" << endl;
+					op1=atoi( numbers.top().c_str() );
+					numbers.pop();
+					op2=atoi( numbers.top().c_str() );
+					numbers.pop();
+					numbers.push( to_string(op2 - op1));
+	    		break;
+				case 3: 
+					//cout << "* operand" << endl;
+					op1=atoi( numbers.top().c_str() );
+					numbers.pop();
+					op2=atoi( numbers.top().c_str() );
+					numbers.pop();
+					numbers.push( to_string(op2 * op1));
+	    		break;
+	    		case 4: 
+					//cout << "/ operand" << endl;
+					op1=atoi( numbers.top().c_str() );
+					numbers.pop();
+					op2=atoi( numbers.top().c_str() );
+					numbers.pop();
+					numbers.push( to_string(op2 / op1));
+	    		break;
+				default: 
+					cout << "ERROR";
+	    		break;
+			}
+		}
 		sIter++;
 	}
-
+	while (numbers.size()>0){
+		cout << numbers.top() << " "  << endl;
+		numbers.pop();
+	}
 }
 
 int main(){
 	evalRPN(ex1);
+	evalRPN(ex2);
+	evalRPN(ex3);
 return 0;
 }
